f095f03aa104c12e2b32b903d71a35fd4321f4d6
Víctor Álvarez <vicventum.dev@gmail.com>
2026-01-06 02:56:17 -0400

    chore(dev-infra): :wrench: Remueve todas las referencias antiguas de Storybook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxt/ui', '@nuxtjs/storybook'],
+    modules: ['@nuxt/ui'],
     css: ['~/assets/css/main.css'],
 
     vite: {
